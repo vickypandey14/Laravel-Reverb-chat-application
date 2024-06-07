e67e185f66305f894a29cdee0a9728a8c44bc075
vickypandey14 <vickypandeyts@gmail.com>
2024-06-07 22:58:28 +0530

    model and migration for messages, and livewire 3 navigate feature

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -3,11 +3,15 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use App\Models\Message;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
     public $user;
+    public $sender_id;
+    public $receiver_id;
+    public $message = '';
 
     public function render()
     {
@@ -16,6 +20,22 @@ class ChatComponent extends Component
 
     public function mount($user_id)
     {
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = $user_id;
         $this->user = User::whereId($user_id)->first();
+    }
+
+    public function sendMessage()
+    {
+        // dd($this->message);
+
+        $chatMessage = new Message();
+        $chatMessage->sender_id = $this->sender_id;
+        $chatMessage->receiver_id = $this->receiver_id;
+        $chatMessage->message = $this->message;
+
+        $chatMessage->save();
+
+        $this->message = '';
     }
 }
--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -3,7 +3,7 @@
         <div class="fixed w-full bg-green-400 h-16 pt-2 text-white flex justify-between shadow-md"
             style="top:0px; overscroll-behavior: none;">
             <!-- back button -->
-            <a href="{{ route('dashboard') }}">
+            <a href="{{ route('dashboard') }}" wire:navigate.hover>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-12 h-12 my-1 text-green-100 ml-2">
                     <path class="text-green-100 fill-current"
                         d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z" />
@@ -34,16 +34,22 @@
         </div>
     </div>
 
-    <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
-        <textarea class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
-            rows="1" placeholder="Message..." style="outline: none;"></textarea>
-        <button class="m-2" style="outline: none;">
-            <svg class="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2" aria-hidden="true"
-                focusable="false" data-prefix="fas" data-icon="paper-plane" role="img"
-                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path fill="currentColor"
-                    d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z" />
-            </svg>
-        </button>
-    </div>
+    <form wire:submit="sendMessage()">
+
+        <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
+
+            <textarea class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none" rows="1" wire:model="message" placeholder="Message..." style="outline: none;"></textarea>
+
+            <button class="m-2" type="submit" style="outline: none;">
+                <svg class="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2" aria-hidden="true"
+                    focusable="false" data-prefix="fas" data-icon="paper-plane" role="img"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z" />
+                </svg>
+            </button>
+
+        </div>
+
+    </form>
+
 </div>
