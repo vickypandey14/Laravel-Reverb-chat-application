7aa4b5a7fc751161e37621de04d3cf91289672bf
Vivek Pandey <101510298+vickypandey14@users.noreply.github.com>
2024-08-10 23:36:05 +0530

    Update navigation.blade.php

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -4,7 +4,7 @@
             href="{{ url('/dashboard') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
-            Dashboard
+           Your Dashboard
         </a>
     @else
         <a
